37861c21c18b597385a333d8139cb9cf9079779d
Samreen Inayat <samreen.inayat@evervent.in>
2026-07-20 22:13:18 +0530

    learn start python

---
+++ 100xdevs-AI/Python/def.py
@@ -0,0 +1,10 @@
+# cook your dish here
+def get_function():
+    choise_game = "rock"
+    choice_computer = "paper"
+    return choise_game
+
+choice = get_function()
+print(choice)  
+
+#  rock 
---
+++ 100xdevs-AI/Python/variable.py
@@ -0,0 +1,4 @@
+choise_game = "rock"
+choice_computer = "paper"
+
+print(choise_game)
