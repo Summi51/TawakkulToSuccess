9bc286f59f8733190a9bc059a07be4faccfeb449
Samreen Inayat <samreen.inayat@evervent.in>
2026-07-25 23:53:29 +0530

    variables and type

---
+++ 100xdevs-AI/Python/type-conversion.py
@@ -0,0 +1,28 @@
+#=======
+name = 'sum'
+print(type(name)) # <class 'str'>
+
+#=======
+# type conversion
+
+age = 23
+print(isinstance(age, int)) # True
+print(isinstance(age, float)) # False
+
+#=======
+age = float(22)
+print(isinstance(age, float)) # True
+
+#=======
+age = '20'
+print(isinstance(age, int)) # False
+
+#=======
+number = '44'
+age = int(number)
+print(isinstance(age, int)) # True
+
+#=======
+# number = 'test'
+# age = int(number)
+# print(isinstance(age, int)) # Runtime Error
--- 100xdevs-AI/Python/variable.py
+++ 100xdevs-AI/Python/variable.py
@@ -6,30 +6,3 @@ print(choise_game)
 #==============
 name = 'sum';  print(name) # one line print statements 
 
-#=======
-print(type(name)) # <class 'str'>
-
-#=======
-# type conversion
-
-age = 23
-print(isinstance(age, int)) # True
-print(isinstance(age, float)) # False
-
-#=======
-age = float(22)
-print(isinstance(age, float)) # True
-
-#=======
-age = '20'
-print(isinstance(age, int)) # False
-
-#=======
-number = '44'
-age = int(number)
-print(isinstance(age, int)) # True
-
-#=======
-# number = 'test'
-# age = int(number)
-# print(isinstance(age, int)) # Runtime Error
