115e0a875c172fb19682cd3469e958caf87466df
Samreen Inayat <samreen.inayat@evervent.in>
2026-07-27 23:46:31 +0530

    python practice

---
+++ 100xdevs-AI/Python/Practice-27-july.py
@@ -0,0 +1,46 @@
+# cook your dish here
+
+# operatore
+# print(4**2) # 16
+# print(5//2) # 2 int
+# print(5/2) # 2.5 float
+
+# boolean operator
+# c1 = True;
+# c2 = False;
+
+# print(not c1) # False
+# print(c1 or c2) # True
+# print(c1 and c2) # False
+
+# or - Find 1st True value
+# print(0 or 1) # 1
+# print(False or 'hay') # 'hay'
+# print('hi' or 'hay') # 'hi'
+# print([] or False) # False
+# print(False or []) # []
+
+# and - Find 1st False value
+# print(0 and 1) # 0
+# print(False and 'hay') # False
+# print('hi' and 'hay') # 'hay'
+# print([] and False) # []
+# print(False and []) # False
+
+#Ternary operatore
+# def is_adult(age):
+#     return True if age >= 18 else False;
+# print(is_adult(19)) # True
+
+#String 
+# name = 'Sum'; 
+# name1 = "Sum1"
+
+# age1 = str(22)
+# print(age1) # 22
+
+# print(""" sum is
+# 28 years old """ )
+
+# sum is
+# 28 years old 
