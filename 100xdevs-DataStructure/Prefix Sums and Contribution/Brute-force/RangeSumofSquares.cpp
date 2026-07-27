b3e35f345ebffb49809595f52b86ae890f7f4d1c
Samreen Inayat <samreen.inayat@evervent.in>
2026-07-27 23:28:04 +0530

    dsa

--- 100xdevs-DataStructure/Prefix Sums and Contribution/Brute-force/RangeSumofSquares.cpp
+++ 100xdevs-DataStructure/Prefix Sums and Contribution/Brute-force/RangeSumofSquares.cpp
@@ -6,29 +6,29 @@ using namespace std;
 int main() {
 	// your code goes here
 
-	int n, q;
+	long long n, q;
 	cin >> n >> q;
 	
-	int arr[n];
-	for(int i=0; i<n; i++){ // O(n)
+	long long arr[n];
+	for(long long i=0; i<n; i++){ // O(n)
 	    cin >> arr[i];
 	   // cout << arr[i] << " ";
 	}
 
-   int p[n];
-   for(int i=0; i<n; i++){
+   long long p[n];
+   for(long long i=0; i<n; i++){
     p[i] = arr[i]*arr[i];
     // cout << p[i] << " ";
 }
 	
-	for(int i=0; i<q; i++){ // O(q*n)
-	    int l, r;
+	for(long long i=0; i<q; i++){ // O(q*n)
+	    long long l, r;
 	    cin >> l >> r;
 	    
 	    l--;
 	    r--;
-	    int sum = 0;
-	    for(int i=l; i<=r; i++){
+	    long long sum = 0;
+	    for(long long i=l; i<=r; i++){
 	        sum +=p[i];
 	    }
 	    
@@ -41,19 +41,19 @@ int main() {
 
 // TLE = O(q*n) = 10^5 * 10^5 = 10^10
 
-// 5
-// 1 2 3 4 5
-// 3
+// 5 3
+// 2 3 1 4 2
 // 1 3
-// 2 4
-// 1 5
+// 2 5
+// 4 4
 
-// 6
-// 9
-// 15
+// 14
+// 30
+// 16
 
 
 // Logic - take new array p[n] 
 // || q loop inside i = l , i<=r loop 
 // || l--, r-- because array start from a1 
 // || l r loop s+=a[i]
+
