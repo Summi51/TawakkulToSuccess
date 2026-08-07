65e1127cf1d8e65ccf45722fc7471bcd90ee63b7
Samreen Inayat <samreen.inayat@evervent.in>
2026-08-07 22:14:30 +0530

    python js input

---
+++ 100xdevs-AI/Python/user-input.py
@@ -0,0 +1,13 @@
+age = input();
+print("Your age is " + age + " years old")
+
+# #++++++++++++++++++
+
+print("What is your age?")
+age = input();
+print("Your age is " + age + " years old");
+
+#++++++++++++++++++
+
+age = input("What is your age? ") # best
+print("Your age is " + age + " years old");
